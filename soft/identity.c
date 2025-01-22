bd9508d323e12a21bdc9a0916f4818d2911f8286
deuskane <deuskane@gmail.com>
2025-01-22 09:41:51 +0100

    identity.c : revert

--- soft/identity.c
+++ soft/identity.c
@@ -33,75 +33,20 @@ extern char PBLAZEPORT[];
 //--------------------------------------
 #define SWITCH    0x00
 #define LED0      0x04
-#define LED1      0x08
-
-//--------------------------------------
-// Global Variable
-//--------------------------------------
-static char cpt;
-
-//--------------------------------------
-// Null function
-//--------------------------------------
-void null (void)
-{
-  // Empty
-}
-
-//--------------------------------------
-// Interrupt Sub Routine
-//--------------------------------------
-void isr (void) __interrupt(1)
-{
-  cpt ++;
-  PORT_WR(LED1,cpt);
-
-  // Workaround in sdcc :
-  // The cpt STORE is update after the context restoration
-  // With null call, we force to make context restoration after the update of cpu variable
-  null();
-}
-
-//--------------------------------------
-// Application Setup
-//--------------------------------------
-// Init counter
-// Send counter to led
-// Enable interuption
-void setup (void)
-{
-  cpt = 0;
-  PORT_WR(LED1,cpt);
-
-  pbcc_enable_interrupt();
-
-  null ();
-}
-
-//--------------------------------------
-// Application Run Loop
-//--------------------------------------
-// Read Switch and write to led
-void loop (void)
-{
-  uint8_t sw = PORT_RD(SWITCH);
-
-#ifdef INVERT_SWITCH
-  sw = ~sw;
-#endif
-  
-  PORT_WR(LED0, sw);
-
-  null ();
-}
 
 //--------------------------------------
 // Main
 //--------------------------------------
-// Arduino Style, Don't modify
 void main()
 {
-  setup ();
   while (1)
-    loop ();
+    {
+      uint8_t sw = PORT_RD(SWITCH);
+
+#ifdef INVERT_SWITCH
+      sw = ~sw;
+#endif
+  
+      PORT_WR(LED0, sw);
+    }
 }
