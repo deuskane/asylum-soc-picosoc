abb50ff7c30c74e4ff347e0dbe2233abc2f435e2
deuskane <deuskane@gmail.com>
2025-01-22 10:15:01 +0100

    reorder switch

--- boards/NanoXplore-DK625V0/pads.py
+++ boards/NanoXplore-DK625V0/pads.py
@@ -1,12 +1,12 @@
 p.addPad('clk_i'             ,{'location':'IOB12_D09P', 'standard' : 'LVCMOS', 'drive' : '2mA'})
 p.addPad('arst_i'            ,{'location':'IOB10_D12N', 'standard' : 'LVCMOS', 'drive' : '2mA'})
                              
-p.addPad('switch_i[0]'       ,{'location':'IOB10_D09P', 'standard' : 'LVCMOS', 'drive' : '2mA'})
-p.addPad('switch_i[1]'       ,{'location':'IOB10_D03P', 'standard' : 'LVCMOS', 'drive' : '2mA'})
-p.addPad('switch_i[2]'       ,{'location':'IOB10_D03N', 'standard' : 'LVCMOS', 'drive' : '2mA'})
-p.addPad('switch_i[3]'       ,{'location':'IOB10_D04P', 'standard' : 'LVCMOS', 'drive' : '2mA'})
-p.addPad('switch_i[4]'       ,{'location':'IOB10_D09N', 'standard' : 'LVCMOS', 'drive' : '2mA'})
-p.addPad('switch_i[5]'       ,{'location':'IOB10_D04N', 'standard' : 'LVCMOS', 'drive' : '2mA'})
+p.addPad('switch_i[0]'       ,{'location':'IOB10_D04P', 'standard' : 'LVCMOS', 'drive' : '2mA'})
+p.addPad('switch_i[1]'       ,{'location':'IOB10_D03N', 'standard' : 'LVCMOS', 'drive' : '2mA'})
+p.addPad('switch_i[2]'       ,{'location':'IOB10_D03P', 'standard' : 'LVCMOS', 'drive' : '2mA'})
+p.addPad('switch_i[3]'       ,{'location':'IOB10_D09P', 'standard' : 'LVCMOS', 'drive' : '2mA'})
+p.addPad('switch_i[4]'       ,{'location':'IOB10_D04N', 'standard' : 'LVCMOS', 'drive' : '2mA'})
+p.addPad('switch_i[5]'       ,{'location':'IOB10_D09N', 'standard' : 'LVCMOS', 'drive' : '2mA'})
                              
 p.addPad('led_o[0]'          ,{'location':'IOB0_D01P' , 'standard' : 'LVCMOS', 'drive' : '2mA'})
 p.addPad('led_o[1]'          ,{'location':'IOB0_D03N' , 'standard' : 'LVCMOS', 'drive' : '2mA'})
