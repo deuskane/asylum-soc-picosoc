53df2399636ddd02b97f8ecbe96888653d6cde58
deuskane <deuskane@gmail.com>
2025-04-15 10:04:53 +0200

    Cosmetics

--- src/OB8_GPIO_user.vhd
+++ src/OB8_GPIO_user.vhd
@@ -6,7 +6,7 @@
 -- Author     : Mathieu Rosiere
 -- Company    : 
 -- Created    : 2017-03-30
--- Last update: 2025-04-14
+-- Last update: 2025-04-15
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -35,37 +35,37 @@ use     work.UART_csr_pkg.all;
 use     work.OB8_GPIO_pkg.all;
 
 entity OB8_GPIO_user is
-    generic (
-    CLOCK_FREQ            : integer  := 50000000;
-    BAUD_RATE             : integer  := 115200;
-    NB_SWITCH             : positive := 8;
-    NB_LED0               : positive := 8;
-    NB_LED1               : positive := 8;
-    SAFETY                : string   := "lock-step"; -- "none" / "lock-step" / "tmr"
-    FAULT_INJECTION       : boolean  := False;
+  generic
+    (CLOCK_FREQ            : integer  := 50000000
+    ;BAUD_RATE             : integer  := 115200
+    ;NB_SWITCH             : positive := 8
+    ;NB_LED0               : positive := 8
+    ;NB_LED1               : positive := 8
+    ;SAFETY                : string   := "lock-step" -- "none" / "lock-step" / "tmr"
+    ;FAULT_INJECTION       : boolean  := False
     
-    TARGET_ADDR_ENCODING  : string := "one_hot";
-    ICN_ALGO_SEL          : string := "or"
+    ;TARGET_ADDR_ENCODING  : string := "one_hot"
+    ;ICN_ALGO_SEL          : string := "or"
     );
-  port (
-    clk_i                 : in  std_logic;
-    arst_b_i              : in  std_logic;
+  port
+    (clk_i                 : in  std_logic
+    ;arst_b_i              : in  std_logic
                           
-    switch_i              : in  std_logic_vector(NB_SWITCH-1 downto 0);
-    led0_o                : out std_logic_vector(NB_LED0  -1 downto 0);
-    led1_o                : out std_logic_vector(NB_LED1  -1 downto 0);
+    ;switch_i              : in  std_logic_vector(NB_SWITCH-1 downto 0)
+    ;led0_o                : out std_logic_vector(NB_LED0  -1 downto 0)
+    ;led1_o                : out std_logic_vector(NB_LED1  -1 downto 0)
                           
-    uart_tx_o             : out std_logic;
-    uart_rx_i             : in  std_logic;
+    ;uart_tx_o             : out std_logic
+    ;uart_rx_i             : in  std_logic
                           
                           
-    it_i                  : in  std_logic;
-    inject_error_i        : in  std_logic_vector(        3-1 downto 0);
-    diff_o                : out std_logic_vector(        3-1 downto 0); -- bit 0 : cpu0 vs cpu1
+    ;it_i                  : in  std_logic
+    ;inject_error_i        : in  std_logic_vector(        3-1 downto 0)
+    ;diff_o                : out std_logic_vector(        3-1 downto 0) -- bit 0 : cpu0 vs cpu1
                                                                         -- bit 1 : cpu1 vs cpu2
                                                                         -- bit 2 : cpu2 vs cpu0
                                  
-    debug_o               : out OB8_GPIO_user_debug_t
+    ;debug_o               : out OB8_GPIO_user_debug_t
     );
 end OB8_GPIO_user;
 
@@ -166,40 +166,40 @@ begin  -- architecture rtl
   -- From level to Val/Ack interruption
   -----------------------------------------------------------------------------
   ins_it_ctrl : entity work.it_ctrl(rtl)
-  port map(
-    clk_i    => clk          ,
-    arstn_i  => arst_b       ,
-    it_i     => it_i         ,
-    it_val_o => cpu_it_val   ,
-    it_ack_i => cpu_it_ack
+    port map
+    (clk_i                => clk          
+    ,arstn_i              => arst_b   
+    ,it_i                 => it_i         
+    ,it_val_o             => cpu_it_val   
+    ,it_ack_i             => cpu_it_ack
     );
   
   -----------------------------------------------------------------------------
   -- CPU 0
   -----------------------------------------------------------------------------
   ins_pbi_OpenBlaze8_0 : entity work.pbi_OpenBlaze8(rtl)
-  port map (
-    clk_i            => clk         ,
-    cke_i            => '1'         ,
-    arstn_i          => arst_b      ,
-    ics_o            => cpu0_ics    ,
-    iaddr_o          => cpu0_iaddr  ,
-    idata_i          => cpu0_idata  ,
-    pbi_ini_o        => cpu0_pbi_ini,
-    pbi_tgt_i        => cpu_pbi_tgt ,
-    interrupt_i      => cpu_it_val  ,
-    interrupt_ack_o  => cpu0_it_ack
+    port map
+    (clk_i                => clk         
+    ,cke_i                => '1'         
+    ,arstn_i              => arst_b      
+    ,ics_o                => cpu0_ics    
+    ,iaddr_o              => cpu0_iaddr  
+    ,idata_i              => cpu0_idata  
+    ,pbi_ini_o            => cpu0_pbi_ini
+    ,pbi_tgt_i            => cpu_pbi_tgt 
+    ,interrupt_i          => cpu_it_val  
+    ,interrupt_ack_o      => cpu0_it_ack
     );
 
   -----------------------------------------------------------------------------
   -- CPU ROM
   -----------------------------------------------------------------------------
   ins_pbi_OpenBlaze8_ROM : entity work.ROM_user(rom)
-    port map (
-      clk_i            => clk      ,
-      cke_i            => cpu_ics  ,
-      address_i        => cpu_iaddr,
-      instruction_o    => cpu_idata
+    port map
+    (clk_i                => clk      
+    ,cke_i                => cpu_ics  
+    ,address_i            => cpu_iaddr
+    ,instruction_o        => cpu_idata
     );
 
   -----------------------------------------------------------------------------
@@ -213,18 +213,18 @@ begin  -- architecture rtl
   -----------------------------------------------------------------------------
   gen_cpu_vote: if SAFETY = "tmr"
   generate
-    cpu_ics       <= ((cpu0_ics     and cpu1_ics    ) or
-                      (cpu1_ics     and cpu2_ics    ) or
-                      (cpu2_ics     and cpu0_ics    ));
-    cpu_iaddr     <= ((cpu0_iaddr   and cpu1_iaddr  ) or
-                      (cpu1_iaddr   and cpu2_iaddr  ) or
-                      (cpu2_iaddr   and cpu0_iaddr  ));
-    cpu_pbi_ini   <= ((cpu0_pbi_ini and cpu1_pbi_ini) or
-                      (cpu1_pbi_ini and cpu2_pbi_ini) or
-                      (cpu2_pbi_ini and cpu0_pbi_ini));
-    cpu_it_ack    <= ((cpu0_it_ack  and cpu1_it_ack ) or
-                      (cpu1_it_ack  and cpu2_it_ack ) or
-                      (cpu2_it_ack  and cpu0_it_ack ));
+    cpu_ics        <= ((cpu0_ics     and cpu1_ics    ) or
+                       (cpu1_ics     and cpu2_ics    ) or
+                       (cpu2_ics     and cpu0_ics    ));
+    cpu_iaddr      <= ((cpu0_iaddr   and cpu1_iaddr  ) or
+                       (cpu1_iaddr   and cpu2_iaddr  ) or
+                       (cpu2_iaddr   and cpu0_iaddr  ));
+    cpu_pbi_ini    <= ((cpu0_pbi_ini and cpu1_pbi_ini) or
+                       (cpu1_pbi_ini and cpu2_pbi_ini) or
+                       (cpu2_pbi_ini and cpu0_pbi_ini));
+    cpu_it_ack     <= ((cpu0_it_ack  and cpu1_it_ack ) or
+                       (cpu1_it_ack  and cpu2_it_ack ) or
+                       (cpu2_it_ack  and cpu0_it_ack ));
   end generate;
 
   gen_cpu_vote_b: if SAFETY /= "tmr"
@@ -240,108 +240,108 @@ begin  -- architecture rtl
   -- From 1 Initiator to N Target
   -----------------------------------------------------------------------------
   ins_pbi_icn : entity work.pbi_icn(rtl)
-    generic map (
-      NB_TARGET            => NB_TARGET,
-      TARGET_ID            => TARGET_ID,
-      TARGET_ADDR_WIDTH    => TARGET_ADDR_WIDTH,
-      TARGET_ADDR_ENCODING => TARGET_ADDR_ENCODING,
-      ALGO_SEL             => ICN_ALGO_SEL
+    generic map
+    (NB_TARGET            => NB_TARGET
+    ,TARGET_ID            => TARGET_ID
+    ,TARGET_ADDR_WIDTH    => TARGET_ADDR_WIDTH
+    ,TARGET_ADDR_ENCODING => TARGET_ADDR_ENCODING
+    ,ALGO_SEL             => ICN_ALGO_SEL
       )
-    port map (
-      clk_i            => clk         ,
-      cke_i            => '1'         ,
-      arst_b_i         => arst_b      ,
-      pbi_ini_i        => cpu_pbi_ini ,
-      pbi_tgt_o        => cpu_pbi_tgt ,
-      pbi_inis_o       => icn_pbi_inis,
-      pbi_tgts_i       => icn_pbi_tgts
+    port map
+    (clk_i                => clk      
+    ,cke_i                => '1'         
+    ,arst_b_i             => arst_b      
+    ,pbi_ini_i            => cpu_pbi_ini 
+    ,pbi_tgt_o            => cpu_pbi_tgt 
+    ,pbi_inis_o           => icn_pbi_inis
+    ,pbi_tgts_i           => icn_pbi_tgts
     );
 
   -----------------------------------------------------------------------------
   -- GPIO 0 - Switch
   -----------------------------------------------------------------------------
   ins_pbi_switch : entity work.pbi_GPIO(rtl)
-    generic map(
-    NB_IO            => NB_SWITCH,
-    DATA_OE_INIT     => CST0(NB_SWITCH-1 downto 0),
-    DATA_OE_FORCE    => CST1(NB_SWITCH-1 downto 0),
-    IT_ENABLE        => false
+    generic map
+    (NB_IO                => NB_SWITCH
+    ,DATA_OE_INIT         => CST0(NB_SWITCH-1 downto 0)
+    ,DATA_OE_FORCE        => CST1(NB_SWITCH-1 downto 0)
+    ,IT_ENABLE            => false
     )
-  port map  (
-    clk_i            => clk           ,
-    cke_i            => '1'           ,
-    arstn_i          => arst_b         ,
-    pbi_ini_i        => icn_pbi_inis(TARGET_SWITCH)   ,
-    pbi_tgt_o        => icn_pbi_tgts(TARGET_SWITCH)   ,
-    data_i           => switch_i      ,
-    data_o           => open          ,
-    data_oe_o        => open          ,
-    interrupt_o      => open          ,
-    interrupt_ack_i  => '0'
+    port map
+    (clk_i                => clk           
+    ,cke_i                => '1'           
+    ,arstn_i              => arst_b         
+    ,pbi_ini_i            => icn_pbi_inis(TARGET_SWITCH)   
+    ,pbi_tgt_o            => icn_pbi_tgts(TARGET_SWITCH)   
+    ,data_i               => switch_i      
+    ,data_o               => open          
+    ,data_oe_o            => open          
+    ,interrupt_o          => open          
+    ,interrupt_ack_i      => '0'
     );
 
   -----------------------------------------------------------------------------
   -- GPIO 1 - LED
   -----------------------------------------------------------------------------
   ins_pbi_led0 : entity work.pbi_GPIO(rtl)
-    generic map(
-    NB_IO            => NB_LED0,
-    DATA_OE_INIT     => CST1(NB_LED0-1 downto 0),
-    DATA_OE_FORCE    => CST1(NB_LED0-1 downto 0),
-    IT_ENABLE        => false
+    generic map
+    (NB_IO                => NB_LED0
+    ,DATA_OE_INIT         => CST1(NB_LED0-1 downto 0)
+    ,DATA_OE_FORCE        => CST1(NB_LED0-1 downto 0)
+    ,IT_ENABLE            => false
     )
-  port map  (
-    clk_i            => clk         ,
-    cke_i            => '1'         ,
-    arstn_i          => arst_b       ,
-    pbi_ini_i        => icn_pbi_inis(TARGET_LED0) ,
-    pbi_tgt_o        => icn_pbi_tgts(TARGET_LED0) ,
-    data_i           => X"00"       ,
-    data_o           => led0_o      ,
-    data_oe_o        => open        ,
-    interrupt_o      => open        ,
-    interrupt_ack_i  => '0'
+    port map
+    (clk_i                => clk         
+    ,cke_i                => '1'         
+    ,arstn_i              => arst_b       
+    ,pbi_ini_i            => icn_pbi_inis(TARGET_LED0) 
+    ,pbi_tgt_o            => icn_pbi_tgts(TARGET_LED0) 
+    ,data_i               => X"00"       
+    ,data_o               => led0_o      
+    ,data_oe_o            => open        
+    ,interrupt_o          => open        
+    ,interrupt_ack_i      => '0'
     );
 
   -----------------------------------------------------------------------------
   -- GPIO 2 - LED
   -----------------------------------------------------------------------------
   ins_pbi_led1 : entity work.pbi_GPIO(rtl)
-    generic map(
-    NB_IO            => NB_LED1,
-    DATA_OE_INIT     => CST1(NB_LED1-1 downto 0),
-    DATA_OE_FORCE    => CST1(NB_LED1-1 downto 0),
-    IT_ENABLE        => false
+    generic map
+    (NB_IO                => NB_LED1
+    ,DATA_OE_INIT         => CST1(NB_LED1-1 downto 0)
+    ,DATA_OE_FORCE        => CST1(NB_LED1-1 downto 0)
+    ,IT_ENABLE            => false
     )
-  port map  (
-    clk_i            => clk         ,
-    cke_i            => '1'         ,
-    arstn_i          => arst_b       ,
-    pbi_ini_i        => icn_pbi_inis(TARGET_LED1) ,
-    pbi_tgt_o        => icn_pbi_tgts(TARGET_LED1) ,
-    data_i           => X"00"       ,
-    data_o           => led1_o      ,
-    data_oe_o        => open        ,
-    interrupt_o      => open        ,
-    interrupt_ack_i  => '0'
+    port map
+    (clk_i                => clk         
+    ,cke_i                => '1'         
+    ,arstn_i              => arst_b       
+    ,pbi_ini_i            => icn_pbi_inis(TARGET_LED1) 
+    ,pbi_tgt_o            => icn_pbi_tgts(TARGET_LED1) 
+    ,data_i               => X"00"       
+    ,data_o               => led1_o      
+    ,data_oe_o            => open        
+    ,interrupt_o          => open        
+    ,interrupt_ack_i      => '0'
     );
 
   -----------------------------------------------------------------------------
   -- UART
   -----------------------------------------------------------------------------
   ins_pbi_uart : entity work.pbi_uart(rtl)
-    generic map(
-      BAUD_RATE      => BAUD_RATE     ,
-      CLOCK_FREQ     => CLOCK_FREQ
-      )
-    port map  (
-      clk_i          => clk           ,
-      arst_b_i       => arst_b        ,
-      pbi_ini_i      => icn_pbi_inis(TARGET_UART)   ,
-      pbi_tgt_o      => icn_pbi_tgts(TARGET_UART)   ,
-      uart_tx_o      => uart_tx_o     ,
-      uart_rx_i      => uart_rx_i     
-      );
+    generic map
+    (BAUD_RATE            => BAUD_RATE     
+    ,CLOCK_FREQ           => CLOCK_FREQ
+     )
+    port map
+    (clk_i                => clk           
+    ,arst_b_i             => arst_b        
+    ,pbi_ini_i            => icn_pbi_inis(TARGET_UART)   
+    ,pbi_tgt_o            => icn_pbi_tgts(TARGET_UART)   
+    ,uart_tx_o            => uart_tx_o     
+    ,uart_rx_i            => uart_rx_i     
+     );
   
   -----------------------------------------------------------------------------
   -- CPU 1
@@ -351,18 +351,18 @@ begin  -- architecture rtl
   generate
     -- Lock Step
     ins_pbi_OpenBlaze8_1 : entity work.pbi_OpenBlaze8(rtl)
-      port map (
-        clk_i            => clk     ,
-        cke_i            => '1'     ,
-        arstn_i          => arst_b   ,
-        ics_o            => cpu1_ics    ,
-        iaddr_o          => cpu1_iaddr  ,
-        idata_i          => cpu1_idata  ,
-        pbi_ini_o        => cpu1_pbi_ini,
-        pbi_tgt_i        => cpu_pbi_tgt ,
-        interrupt_i      => cpu_it_val  ,
-        interrupt_ack_o  => cpu1_it_ack
-        );
+      port map
+      (clk_i                => clk           
+      ,cke_i                => '1'     
+      ,arstn_i              => arst_b   
+      ,ics_o                => cpu1_ics    
+      ,iaddr_o              => cpu1_iaddr  
+      ,idata_i              => cpu1_idata  
+      ,pbi_ini_o            => cpu1_pbi_ini
+      ,pbi_tgt_i            => cpu_pbi_tgt 
+      ,interrupt_i          => cpu_it_val  
+      ,interrupt_ack_o      => cpu1_it_ack
+       );
 
     diff(0) <= '1' when (   (cpu0_ics           /= cpu1_ics          )
                          or (cpu0_iaddr         /= cpu1_iaddr        )
@@ -398,18 +398,18 @@ begin  -- architecture rtl
   generate
     -- TMR
     ins_pbi_OpenBlaze8_2 : entity work.pbi_OpenBlaze8(rtl)
-      port map (
-        clk_i            => clk     ,
-        cke_i            => '1'     ,
-        arstn_i          => arst_b   ,
-        ics_o            => cpu2_ics    ,
-        iaddr_o          => cpu2_iaddr  ,
-        idata_i          => cpu2_idata  ,
-        pbi_ini_o        => cpu2_pbi_ini,
-        pbi_tgt_i        => cpu_pbi_tgt ,
-        interrupt_i      => cpu_it_val  ,
-        interrupt_ack_o  => cpu2_it_ack
-        );
+      port map
+      (clk_i                => clk           
+      ,cke_i                => '1'     
+      ,arstn_i              => arst_b   
+      ,ics_o                => cpu2_ics    
+      ,iaddr_o              => cpu2_iaddr  
+      ,idata_i              => cpu2_idata  
+      ,pbi_ini_o            => cpu2_pbi_ini
+      ,pbi_tgt_i            => cpu_pbi_tgt 
+      ,interrupt_i          => cpu_it_val  
+      ,interrupt_ack_o      => cpu2_it_ack
+       );
 
     diff(1) <= '1' when (   (cpu1_ics           /= cpu2_ics          )
                          or (cpu1_iaddr         /= cpu2_iaddr        )
