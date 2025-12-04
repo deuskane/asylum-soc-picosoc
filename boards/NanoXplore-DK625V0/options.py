339da6cb79b0abcc5ee2f5a9ba82894c8c8f2d34
deuskane <deuskane@gmail.com>
2025-12-04 14:53:06 +0100

    Add workaround for impulse 25.1 release

--- boards/NanoXplore-DK625V0/options.py
+++ boards/NanoXplore-DK625V0/options.py
@@ -3,5 +3,5 @@ from nxpython import *
 #p.addMappingDirective(getModels('*ram_1r1w*'), 'RAM', 'DFF')
 
 p.setOptions({'ManageAsynchronousReadPort' : 'Yes',
-              'AllowUnconfiguredIOs'       : 'Yes'
+              'AllowUnconfiguredIOs'       : 'Yes' # It's workaround with bug in 25.1.0.6
 })
