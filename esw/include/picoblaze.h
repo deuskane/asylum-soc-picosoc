d1760094b44bf43b13cb9ad522275cafe88c0e3b
deuskane <deuskane@gmail.com>
2026-05-14 09:12:38 +0200

    Rename target to select cpu model

--- esw/include/picoblaze.h
+++ esw/include/picoblaze.h
@@ -12,18 +12,28 @@
 // Revisions  :
 // Date        Version  Author   Description
 // 2025-06-14  1.0      mrosiere Created
+// 2026-05-14  1.1      mrosiere Support others picoblaze cores 
+//                               (but keep the same interface)
 //-----------------------------------------------------------------------------
 
 #ifndef _picoblaze_h_
 #define _picoblaze_h_
 
+#include <stdint.h>
 
 //--------------------------------------
 // Port Macro
 //--------------------------------------
-extern volatile char PBLAZEPORT[];
+#ifdef picoblaze
+
+// This variable is defined in the picoblaze compiler
+extern volatile uint8_t PBLAZEPORT[];
+#else
+#define PBLAZEPORT ((volatile uint8_t*)0)
+#endif
 
 #define PORT_WR(_BA_,_OFFSET_,_DATA_) PBLAZEPORT[(_BA_)+(_OFFSET_)] = (_DATA_)
 #define PORT_RD(_BA_,_OFFSET_)        PBLAZEPORT[(_BA_)+(_OFFSET_)]
+
 
 #endif
